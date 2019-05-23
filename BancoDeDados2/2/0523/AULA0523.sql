7253cfe162487fea375ad869e68b2ee3f34f1057
vitor barbosa <viitorrsbarbosa@gmail.com>
2019-05-23 20:34:35 -0300

    material da aula de banco sobre expressões regulares

--- BancoDeDados2/2/0523/AULA0523.sql
+++ BancoDeDados2/2/0523/AULA0523.sql
@@ -1,4 +1,36 @@
-select nm_func, nm_sobrenome, dt_nasc from func
-where regexp_like(to_char(dt_nasc,'yyyy'),'^197[0-5]$');
 
-select nm_func, nm_sobrenome, dt_nasc from func
+
+
+
+select nm_func, nm_sobrenome, dt_nasc from func where nm_func = 'joao' or nm_func = 'maria';
+
+CREATE TABLE TOYS(
+ID NUMBER,
+DESCR VARCHAR2(20));
+INSERT INTO TOYS VALUES (1,'TOY1');
+INSERT INTO TOYS VALUES (2,'T1OY2');
+INSERT INTO TOYS VALUES (3,'1TOY1');
+INSERT INTO TOYS VALUES (4,'999');
+INSERT INTO TOYS VALUES (5,'8888');
+INSERT INTO TOYS VALUES (6,'TOY2007');
+COMMIT;
+
+SELECT DESCR FROM TOYS
+WHERE REGEXP_LIKE(DESCR,'[^[:digit:]]');
+
+SELECT REGEXP_COUNT('Rodrigo Righetti','i',2,'c') CONTADOR from dual;
+/*
+realiza busca do caractere 'i'
+iniciando a busca a partir da
+segunda posição e apenas caracteres
+minúsculos devido ao 'c'
+*/
+SELECT REGEXP_COUNT('Ricardo Rezende','R',1,'c') CONTADOR from dual;
+
+SELECT REGEXP_REPLACE('RodrigoRighetti','(){0,}',' ') from dual;
+SELECT REGEXP_REPLACE('Rodrigo Righetti','(){1,}','%') from dual;
+SELECT REGEXP_REPLACE('Rodrigo Righetti','()|( )|(%){1,}','') from dual;
+
+SELECT REGEXP_REPLACE(DESCR,'[^[:alpha:]]')FROM TOYS;
+
+SELECT REGEXP_INSTR('ASTRINGWITHNUMBERS1234 ETC','[^[:alpha]]') FROM DUAL;
