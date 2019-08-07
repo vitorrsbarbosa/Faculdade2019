a16d93862764d0ad126b158bbe8301e6b9f4b056
vitor barbosa <viitorrsbarbosa@gmail.com>
2019-08-07 00:15:11 -0300

    iniciando projeto da rocketseat

---
+++ aula01/backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const DevController = require('./controllers/DevController');
+
+const routes = express.Router();
+
+/*
+routes.get('/',(req, res) => {
+    return res.json({ message: `hey, ${req.query.name} shounen` });
+});
+*/
+
+routes.post('/devs',DevController.store);
+
+module.exports = routes;
