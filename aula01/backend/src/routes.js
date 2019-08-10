158ea8c2d2d34c9191b1db812f17e31c39a1bc86
vitor barbosa <viitorrsbarbosa@gmail.com>
2019-08-09 21:20:58 -0300

    continuação dos estudos sobre react

--- aula01/backend/src/routes.js
+++ aula01/backend/src/routes.js
@@ -3,11 +3,10 @@ const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-/*
 routes.get('/',(req, res) => {
     return res.json({ message: `hey, ${req.query.name} shounen` });
 });
-*/
+
 
 routes.post('/devs',DevController.store);
 
