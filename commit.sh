c7126b28ec621120b056592294a88d18e09c8e0a
Guukiza <viitorrsbarbosa@gmail.com>
2019-10-27 19:25:09 -0300

    atualizando arquivo de commit

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 MENSAGEM="$1"
-git config user.name vitorbarbosa
+git config user.name Guukiza
 git config user.email viitorrsbarbosa@gmail.com
 git add .
 git commit -m "$MENSAGEM"
-git push origin BancoDeDados
+git push origin PesquisaOperacional
